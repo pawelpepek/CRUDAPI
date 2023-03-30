44b730b4165ed7bbcaa9af155578f82b32974193
Paweł Pepek <pepek@riskout.pl>
2023-03-30 22:35:17 +0200

    ProductController code cleaning

--- CRUDAPI/Controllers/ProductController.cs
+++ CRUDAPI/Controllers/ProductController.cs
@@ -12,37 +12,25 @@ public class ProductController : ControllerBase
     private readonly ICRUDService<Product> _crud;
 
     public ProductController(ICRUDService<Product> crud)
-    {
-        _crud = crud;
-    }
+        => _crud = crud;
 
     [HttpPost]
     public async Task<int> AddNew([FromBody] CreateProductDto dto)
-    {
-        return await _crud.AddEntity(dto);
-    }
+        => await _crud.AddEntity(dto);
 
     [HttpGet]
     public async Task<List<ProductDto>> GetAll()
-    {
-        return await _crud.GetDtos<ProductDto>();
-    }
+        => await _crud.GetDtos<ProductDto>();
 
     [HttpGet("{id}")]
     public async Task<ProductDto> GetProduct(int id)
-    {
-        return await _crud.GetDtoById<ProductDto>(id);
-    }
+        => await _crud.GetDtoById<ProductDto>(id);
 
     [HttpPut("{id}")]
     public async Task Update(int id, [FromBody] CreateProductDto dto)
-    {
-        await _crud.UpdateEntity(id, dto);
-    }
+        => await _crud.UpdateEntity(id, dto);
 
     [HttpDelete("{id}")]
     public async Task Remove(int id)
-    {
-        await _crud.RemoveEntity(id);
-    }
+        => await _crud.RemoveEntity(id);
 }
