837b9f38bd849fce04fd83b27b850bd5de3c334d
Paweł Pepek <pepek@riskout.pl>
2023-03-30 22:30:36 +0200

    Product endpoints

--- CRUDAPI/Controllers/ProductController.cs
+++ CRUDAPI/Controllers/ProductController.cs
@@ -16,9 +16,33 @@ public class ProductController : ControllerBase
         _crud = crud;
     }
 
+    [HttpPost]
+    public async Task<int> AddNew([FromBody] CreateProductDto dto)
+    {
+        return await _crud.AddEntity(dto);
+    }
+
     [HttpGet]
     public async Task<List<ProductDto>> GetAll()
     {
         return await _crud.GetDtos<ProductDto>();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ProductDto> GetProduct(int id)
+    {
+        return await _crud.GetDtoById<ProductDto>(id);
+    }
+
+    [HttpPut("{id}")]
+    public async Task Update(int id, [FromBody] CreateProductDto dto)
+    {
+        await _crud.UpdateEntity(id, dto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Remove(int id)
+    {
+        await _crud.RemoveEntity(id);
     }
 }
