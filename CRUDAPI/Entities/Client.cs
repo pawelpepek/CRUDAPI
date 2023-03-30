96465e16103efb4f6941822156ef58545f3c84d3
Paweł Pepek <pepek@riskout.pl>
2023-03-30 23:35:45 +0200

    Client LastName property fix

--- CRUDAPI/Entities/Client.cs
+++ CRUDAPI/Entities/Client.cs
@@ -4,7 +4,7 @@ public class Client:IIdentifiable
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
-    public int LastName { get; set; }
+    public string LastName { get; set; }
 
     public virtual List<Order> Orders { get; set; }
 
---
+++ CRUDAPI/Migrations/20230330213436_ClientLastNameFix.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDAPI.Migrations
+{
+    public partial class ClientLastNameFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Client",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LastName",
+                table: "Client",
+                type: "integer",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
