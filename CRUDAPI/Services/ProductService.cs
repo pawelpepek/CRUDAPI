ce6e4db2cd3fafd69753dafc79b8616d5ad1300f
Paweł Pepek <pepek@riskout.pl>
2023-04-03 23:29:36 +0200

    ProductService code cleaning

--- CRUDAPI/Services/ProductService.cs
+++ CRUDAPI/Services/ProductService.cs
@@ -14,27 +14,25 @@ public class ProductService : CRUDService<Product>
     public ProductService(ApplicationDbContext context, IMapper mapper)
       : base(context, mapper)
     {
-        _creator.SetEntityValidationAction
-        (
-            (entity) =>
-            {
-                var duplicate = _set.FirstOrDefault(
-                    p => p.Name.ToLower() == entity.Name.ToLower() && p.Quality == entity.Quality);
-
-                if (duplicate != null)
-                {
-                    throw new CustomException($"Produkt o nazwie ${entity.Name} istnieje już w bazie danych!");
-                }
-
-                duplicate = _set.FirstOrDefault(p => p.Code == entity.Code && p.Quality == entity.Quality);
-
-                if (duplicate != null)
-                {
-                    throw new CustomException($"Produkt o kodzie ${entity.Code} istnieje już w bazie danych!");
-                }
-            }
-         );
-
+        _creator.SetEntityValidationAction(ValidateNewEntity);
         _reader.SetIncludeFunction((entities) => entities.Include(p => p.ProductAmounts));
+    }
+
+    private void ValidateNewEntity(Product entity)
+    {
+        var duplicate = _set.FirstOrDefault(
+            p => p.Name.ToLower() == entity.Name.ToLower() && p.Quality == entity.Quality);
+
+        if (duplicate != null)
+        {
+            throw new CustomException($"Produkt o nazwie ${entity.Name} istnieje już w bazie danych!");
+        }
+
+        duplicate = _set.FirstOrDefault(p => p.Code == entity.Code && p.Quality == entity.Quality);
+
+        if (duplicate != null)
+        {
+            throw new CustomException($"Produkt o kodzie ${entity.Code} istnieje już w bazie danych!");
+        }
     }
 }
